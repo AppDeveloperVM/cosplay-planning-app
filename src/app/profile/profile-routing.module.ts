5381675868ee821714dce8c2aa027a5bf5c2cedb
AppDeveloperVM <vicmr1994@gmail.com>
2020-04-16 13:07:49 +0200

    Updated profile-routing ROUTES

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -5,8 +5,61 @@ import { ProfilePage } from './profile.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: ProfilePage,
+    children : [
+      {
+        path: 'cosplays',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./cosplays/cosplays.module').then( m => m.CosplaysPageModule)
+          },
+          {
+            path: 'my-cosplays',
+            loadChildren: () => import('./cosplays/my-cosplays/my-cosplays.module').then( m => m.MyCosplaysPageModule)
+          },
+          {
+            path: ':cosplayId',
+            // tslint:disable-next-line: max-line-length
+            loadChildren: () => import('./cosplays/my-cosplays/cosplay-details/cosplay-details.module').then( m => m.CosplayDetailsPageModule)
+          },
+          {
+            path: 'new',
+            loadChildren: () => import('./cosplays/my-cosplays/new-cosplay/new-cosplay.module').then( m => m.NewCosplayPageModule)
+          },
+          {
+            path: 'edit/:cosplayId',
+            loadChildren: () => import('./cosplays/my-cosplays/edit-cosplay/edit-cosplay.module').then( m => m.EditCosplayPageModule)
+          }
+        ]
+      },
+      {
+        path: 'conventions',
+        children:
+        [
+          {
+            path: '',
+            loadChildren: () => import('./conventions/conventions.module').then( m => m.ConventionsPageModule)
+          },
+          {
+            path: ':conId',
+            loadChildren: () => import('./conventions/con-detail/con-detail.module').then( m => m.ConDetailPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/profile/tabs/cosplays',
+        pathMatch: 'full'
+      }
+    ]
+  }
+  ,
+  {
     path: '',
-    component: ProfilePage
+    redirectTo: '/profile/tabs/cosplays',
+    pathMatch: 'full'
   }
 ];
 
