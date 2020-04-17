c74f1f26ae0b8ff5855913eaeffd09a334d58523
AppDeveloperVM <vicmr1994@gmail.com>
2020-04-17 19:15:23 +0200

    Added conventions.service.ts

---
+++ src/app/profile/conventions/conventions.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Convention } from './conventions.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConventionsService {
+  private _conventions: Convention[] = [
+    new Convention('c1','Japan Weekend Septiembre','Spain','https://static-2.ivoox.com/canales/1/7/0/4/6791535494071_MD.jpg'),
+  ]
+
+  get cons(){
+    return [...this._conventions];
+  }
+
+  constructor() { }
+}
