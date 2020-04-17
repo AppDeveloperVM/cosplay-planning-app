68e1c24146bc584c240ca4be729b532d4b310b2b
AppDeveloperVM <vicmr1994@gmail.com>
2020-04-17 13:41:26 +0200

    Created cosplay.model.ts

---
+++ src/app/profile/cosplays/cosplay.model.ts
@@ -0,0 +1,12 @@
+export class Cosplay {
+    constructor(
+        public id: string,
+        public characterName: string,
+        public description: string,
+        public imageUrl: string,
+        public series: string,
+        public funds: number,
+        public percentComplete: string,
+        public status: boolean
+    ) {}
+}
