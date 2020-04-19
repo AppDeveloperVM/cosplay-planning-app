c5d8a9d8bb4710b3c9674642b5296eabe7299392
AppDeveloperVM <vicmr1994@gmail.com>
2020-04-19 18:43:20 +0200

    New "cosplay details" functions - Load, Edit
    Load cosplay data
    
    Edit cosplay data

--- src/app/profile/cosplays/my-cosplays/cosplay-details/cosplay-details.page.ts
+++ src/app/profile/cosplays/my-cosplays/cosplay-details/cosplay-details.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Cosplay } from '../../cosplay.model';
+import { CosplaysService } from '../../cosplays.service';
 
 @Component({
   selector: 'app-cosplay-details',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cosplay-details.page.scss'],
 })
 export class CosplayDetailsPage implements OnInit {
+  cosplay: Cosplay;
 
-  constructor() { }
+  constructor(private router: Router, private navCtrl: NavController, private route: ActivatedRoute, private cosplaysService: CosplaysService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('cosplayId')) {
+        this.navCtrl.navigateBack('/profile/tabs/cosplays/my-cosplays');
+        return;
+      }
+      // load the cosplay
+      this.cosplay = this.cosplaysService.getCosplay(paramMap.get('cosplayId'));
+    });
+  }
+
+  onEditCosplay() {
+    // this.router.navigateByUrl('/profile/tabs/cosplays/my-cosplays');
+    this.navCtrl.navigateBack('/profile/tabs/cosplays/my-cosplays');
   }
 
 }
