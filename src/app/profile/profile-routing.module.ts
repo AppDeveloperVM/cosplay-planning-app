6ad261bbf97d068752aacebba924e3b1c6ff8dde
AppDeveloperVM <vicmr1994@gmail.com>
2020-04-23 17:48:00 +0200

    Profile-routing.module.ts - updated paths

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -7,67 +7,6 @@ const routes: Routes = [
   {
     path: '',
     component: ProfilePage
-    /*path: 'tabs',
-    component: ProfilePage,
-    children : [
-      {
-        path: 'cosplays',
-        children: [
-          {
-            path: '',
-            loadChildren: () => import('./cosplays/cosplays.module').then( m => m.CosplaysPageModule)
-          },
-          {
-            path: 'my-cosplays',
-            children : [
-              {
-                path: '',
-                loadChildren: () => import('./cosplays/my-cosplays/my-cosplays.module').then( m => m.MyCosplaysPageModule)
-              },
-              {
-                path: 'new',
-                loadChildren: () => import('./cosplays/my-cosplays/new-cosplay/new-cosplay.module').then( m => m.NewCosplayPageModule)
-              },
-              {
-                path: ':cosplayId',
-                // tslint:disable-next-line: max-line-length
-                loadChildren: () => import('./cosplays/my-cosplays/cosplay-details/cosplay-details.module').then( m => m.CosplayDetailsPageModule)
-              },
-              {
-                path: 'edit/:cosplayId',
-                loadChildren: () => import('./cosplays/my-cosplays/edit-cosplay/edit-cosplay.module').then( m => m.EditCosplayPageModule)
-              }
-            ]
-          }
-        ]
-      },
-      {
-        path: 'conventions',
-        children:
-        [
-          {
-            path: '',
-            loadChildren: () => import('./conventions/conventions.module').then( m => m.ConventionsPageModule)
-          },
-          {
-            path: ':conId',
-            loadChildren: () => import('./conventions/con-detail/con-detail.module').then( m => m.ConDetailPageModule)
-          }
-        ]
-      },
-      {
-        path: '',
-        redirectTo: '/profile/tabs/cosplays/my-cosplays',
-        pathMatch: 'full'
-      }
-    ]
-  }
-  ,
-  {
-    path: '',
-    redirectTo: '/profile/tabs/cosplays/my-cosplays',
-    pathMatch: 'full'
-    */
   }
   ]
 ;
