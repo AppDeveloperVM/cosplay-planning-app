686ad58d409c1fd7edfde42fa8747aa07f4763d4
AppDeveloperVM <vicmr1994@gmail.com>
2020-04-26 19:30:43 +0200

    Added Segmented button on Cosplays page
    Segmented button:
    Unfinished /Finished

--- src/app/main/cosplays/my-cosplays/my-cosplays.page.ts
+++ src/app/main/cosplays/my-cosplays/my-cosplays.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CosplaysService } from '../cosplays.service';
 import { Cosplay } from '../cosplay.model';
+import { SegmentChangeEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-my-cosplays',
@@ -16,6 +17,10 @@ export class MyCosplaysPage implements OnInit {
   ngOnInit() {
     this.loadedCosplays = this.cosplaysService.cosplays;
     this.listedLoadedCosplays = this.loadedCosplays.slice(0);
+  }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);
   }
 
 }
