25e3f1b0bfcef04b444a10603e52a3cc976aed76
AppDeveloperVM <vicmr1994@gmail.com>
2020-05-10 17:54:50 +0200

    Update cosplay-groups.page.ts

--- src/app/main/cosplays/cosplay-groups/cosplay-groups.page.ts
+++ src/app/main/cosplays/cosplay-groups/cosplay-groups.page.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CosplayGroup } from './cosplay-group.model';
 import { CosplayGroupService } from './cosplay-group.service';
 
-@Component({
+@Component(
+  {
   selector: 'app-cosplay-groups',
   templateUrl: './cosplay-groups.page.html',
   styleUrls: ['./cosplay-groups.page.scss'],
-})
+}
+)
+
 export class CosplayGroupsPage implements OnInit {
   cosplaygroups: CosplayGroup[];
 
