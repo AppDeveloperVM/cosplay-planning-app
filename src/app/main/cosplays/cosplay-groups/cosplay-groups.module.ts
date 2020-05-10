e27436214a3a315fa65b55d67ef89013ce91c94a
AppDeveloperVM <vicmr1994@gmail.com>
2020-05-10 17:25:28 +0200

    Fixed use of <app-cosplay-group-item>
    fixed by adding CosplayGrouItemComponent on Declaration
    
    @NgModule({
      imports: [
        ...
      ],
      declarations: [CosplayGroupsPage, CosplayGroupItemComponent]    <  --
    })

--- src/app/main/cosplays/cosplay-groups/cosplay-groups.module.ts
+++ src/app/main/cosplays/cosplay-groups/cosplay-groups.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { CosplayGroupsPageRoutingModule } from './cosplay-groups-routing.module';
 
 import { CosplayGroupsPage } from './cosplay-groups.page';
+import { CosplayGroupItemComponent } from './cosplay-group-item/cosplay-group-item.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { CosplayGroupsPage } from './cosplay-groups.page';
     IonicModule,
     CosplayGroupsPageRoutingModule
   ],
-  declarations: [CosplayGroupsPage]
+  declarations: [CosplayGroupsPage, CosplayGroupItemComponent]
 })
 export class CosplayGroupsPageModule {}
