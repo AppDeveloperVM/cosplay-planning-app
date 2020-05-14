9336448d8996868e14c67ef9386ac0fac533b18f
AppDeveloperVM <vicmr1994@gmail.com>
2020-05-14 18:53:06 +0200

    Auth Form Functionality added
    - Switch from 'Login' to 'Singup' ( actually showed with a text )
    - Data Submitable ( not processed in backend )

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { Form, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -10,6 +11,7 @@ import { LoadingController } from '@ionic/angular';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) { }
 
@@ -29,6 +31,26 @@ export class AuthPage implements OnInit {
         this.router.navigateByUrl('/main/tabs/cosplays/my-cosplays');
       } , 1000);
      });
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) { //if is false
+      return;
+    }
+    const email = form.value.email;
+    const passwd = form.value.passwd;
+    console.log(email, passwd);
+
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
+
   }
 
 }
