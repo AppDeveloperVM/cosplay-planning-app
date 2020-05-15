b150dfc7c6201f884dd7227c1b978aff2198e191
AppDeveloperVM <vicmr1994@gmail.com>
2020-05-15 21:26:34 +0200

    Updated declarations and entrycomponents

--- src/app/main/cosplays/cosplay-groups/cosplay-group-details/cosplay-group-details.module.ts
+++ src/app/main/cosplays/cosplay-groups/cosplay-group-details/cosplay-group-details.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { CosplayGroupDetailsPageRoutingModule } from './cosplay-group-details-routing.module';
 
 import { CosplayGroupDetailsPage } from './cosplay-group-details.page';
+import { CosplayGroupSendRequestComponent } from '../cosplay-group-send-request/cosplay-group-send-request.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { CosplayGroupDetailsPage } from './cosplay-group-details.page';
     IonicModule,
     CosplayGroupDetailsPageRoutingModule
   ],
-  declarations: [CosplayGroupDetailsPage]
+  declarations: [CosplayGroupDetailsPage, CosplayGroupSendRequestComponent],
+  entryComponents: [CosplayGroupSendRequestComponent]
 })
 export class CosplayGroupDetailsPageModule {}
