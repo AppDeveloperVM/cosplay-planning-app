8f8ffa30a07b6ac5fa0f9ff581c3f7aa5d33505f
AppDeveloperVM <vicmr1994@gmail.com>
2020-05-15 18:53:11 +0200

    Updated "New Cosplay Group" Form - ReactiveForm
    ReactiveFormModule
    
    imported in "new-cosplay-group.module.ts"
    use with " new FormControl(.. , { } ) "

--- src/app/main/cosplays/cosplay-groups/new-cosplay-group/new-cosplay-group.page.ts
+++ src/app/main/cosplays/cosplay-groups/new-cosplay-group/new-cosplay-group.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-cosplay-group',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-cosplay-group.page.scss'],
 })
 export class NewCosplayGroupPage implements OnInit {
+  form: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      place: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      dateFrom: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [ Validators.required]
+      }),
+      dateTo: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [ Validators.required]
+      })
+    });
+  }
+
+  onCreateOffer() {
+    console.log('Creating offered place');
   }
 
 }
