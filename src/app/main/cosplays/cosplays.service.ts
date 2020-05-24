622d68184ee1eb8a241794b8b1a730d1c74e689d
AppDeveloperVM <vicmr1994@gmail.com>
2020-05-24 18:49:56 +0200

    New Function.."Add cosplay"..for cosplaygroups - not finished

--- src/app/main/cosplays/cosplays.service.ts
+++ src/app/main/cosplays/cosplays.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Cosplay } from './cosplay.model';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CosplaysService {
   private _cosplays: Cosplay[] = [
-  new Cosplay('c1', 'Samatoki', 'MTC rapper', 'https://pbs.twimg.com/media/DluGJLAUYAEdcIT?format=jpg&name=small', 'Hypmic', 0, '0', false),
-  new Cosplay('c2', 'Jyuuto', 'MTC rapper', 'https://pbs.twimg.com/media/DluGVivU0AA0U87?format=jpg&name=small', 'Hypmic', 0, '0', false),
-  new Cosplay('c3', 'Riou', 'MTC rapper', 'https://pbs.twimg.com/media/DluGfijU8AA0XcX?format=jpg&name=small', 'Hypmic', 0, '0', false)
+  new Cosplay('c1', 'Samatoki', 'MTC rapper', 'https://pbs.twimg.com/media/DluGJLAUYAEdcIT?format=jpg&name=small', 'Hypmic', 0, '0', false,'user1'),
+  new Cosplay('c2', 'Jyuuto', 'MTC rapper', 'https://pbs.twimg.com/media/DluGVivU0AA0U87?format=jpg&name=small', 'Hypmic', 0, '0', false, 'user1'),
+  new Cosplay('c3', 'Riou', 'MTC rapper', 'https://pbs.twimg.com/media/DluGfijU8AA0XcX?format=jpg&name=small', 'Hypmic', 0, '0', false, 'user1')
   ];
+
+  constructor(private authService: AuthService) { }
 
   get cosplays() {
     return [...this._cosplays];
@@ -21,5 +24,20 @@ export class CosplaysService {
     return {...this._cosplays.find(c => c.id === id)};
   }
 
-  constructor() { }
+  // Add a new cosplay.. ? Pero yo necesito un nuevo cosplay Group character request
+  setCosplayGroupRequest(id: string, characterName: string, description: string, imageUrl: string, series: string, funds: number, percentComplete: string, status: boolean, userId: string) {
+    const newCosplayRequest = new Cosplay(
+        Math.random().toString(),
+        characterName,
+        description,
+        'https://pbs.twimg.com/media/DluGJLAUYAEdcIT?format=jpg&name=small',
+        series,
+        funds,
+        percentComplete,
+        status,
+        this.authService.userId
+    );
+    this._cosplays.push(newCosplayRequest);
+  }
+
 }
