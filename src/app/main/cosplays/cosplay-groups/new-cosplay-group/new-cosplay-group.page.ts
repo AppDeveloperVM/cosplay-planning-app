97816f70aa5f9a7ba3048dc1af2a3a138f718ebe
AppDeveloperVM <vicmr1994@gmail.com>
2020-05-25 20:22:46 +0200

    New CosplayGroup Form updated - input date added

--- src/app/main/cosplays/cosplay-groups/new-cosplay-group/new-cosplay-group.page.ts
+++ src/app/main/cosplays/cosplay-groups/new-cosplay-group/new-cosplay-group.page.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { CosplayGroup } from '../cosplay-group.model';
 
 @Component({
   selector: 'app-new-cosplay-group',
@@ -8,10 +11,18 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class NewCosplayGroupPage implements OnInit {
   form: FormGroup;
+  @Input() selectedCosplayGroup: CosplayGroup;
+  @Input() selectedMode: 'select' | 'random';
+  startDate: string;
+  endDate: string;
 
   constructor() { }
 
   ngOnInit() {
+    const availableFrom = new Date(this.selectedCosplayGroup.availableFrom);
+    const availableTo = new Date(this.selectedCosplayGroup.availableTo);
+    
+
     this.form = new FormGroup({
       title: new FormControl(null, {
         updateOn: 'blur',
@@ -36,8 +47,9 @@ export class NewCosplayGroupPage implements OnInit {
     });
   }
 
-  onCreateOffer() {
+  onSendCosplayGroupRequest() {
     console.log(this.form);
   }
+
 
 }
