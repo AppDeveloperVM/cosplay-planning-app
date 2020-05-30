cde671f9c0ca0e7c2e34427f4512f4900ed88876
AppDeveloperVM <vicmr1994@gmail.com>
2020-05-30 18:23:53 +0200

    Added Filter ALL - FINISHED COSPLAYS in .ts

--- src/app/main/cosplays/my-cosplays/my-cosplays.page.ts
+++ src/app/main/cosplays/my-cosplays/my-cosplays.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CosplaysService } from '../cosplays.service';
 import { Cosplay } from '../cosplay.model';
 import { SegmentChangeEventDetail } from '@ionic/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-my-cosplays',
@@ -11,15 +12,31 @@ import { SegmentChangeEventDetail } from '@ionic/core';
 export class MyCosplaysPage implements OnInit {
   loadedCosplays: Cosplay[];
   listedLoadedCosplays: Cosplay[];
+  relevantCosplays: Cosplay[];
 
-  constructor(private cosplaysService: CosplaysService) { }
+  constructor(
+    private cosplaysService: CosplaysService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
     this.loadedCosplays = this.cosplaysService.cosplays;
+
+    this.relevantCosplays = this.loadedCosplays;
+
     this.listedLoadedCosplays = this.loadedCosplays.slice(0);
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    if (event.detail.value === 'all') {
+      this.relevantCosplays = this.loadedCosplays; // show everything (?)
+    } else {
+      // filtro - a mostrar tras elegir segundo segmented button on main
+      this.relevantCosplays = this.loadedCosplays.filter(
+        cosplay => cosplay.status !== true // checking status
+      );
+    }
+    this.listedLoadedCosplays = this.relevantCosplays.slice(0);
     console.log(event.detail);
   }
 
