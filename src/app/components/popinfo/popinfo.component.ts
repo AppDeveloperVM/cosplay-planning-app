b4945812b209200a700083d85169623da6b99e6e
AppDeveloperV <vicmr1994@gmail.com>
2020-06-09 22:34:21 +0200

    Updated Notifications example list

--- src/app/components/popinfo/popinfo.component.ts
+++ src/app/components/popinfo/popinfo.component.ts
@@ -12,10 +12,10 @@ export class PopinfoComponent implements OnInit {
   items = this.notif_count;
 
   notifications = [
-    {"id":1,"name":"Licensed Frozen Hat","description":"Incidunt et magni","price":"170.00","quantity":56840},
-    {"id":2,"name":"Rustic Concrete Chicken","description":"Sint libero mollitia","price":"302.00","quantity":9358},
-    {"id":3,"name":"Fantastic Metal Computer","description":"In consequuntur cupiditat","price":"279.00","quantity":90316},
-    {"id":4,"name":"Refined Concrete Chair","description":"Saepe nemo praesentium","price":"760.00","quantity":5899}
+    {"id":1,"name":"JhonMarcus","type":"comment","text":"I really liked your photo, you are improving!"},
+    {"id":2,"name":"Elizabeth duck looking","type":"comment","text":"Are you planning on buying another t-shirt?"},
+    {"id":3,"name":"Fantastic Metal Computer","type":"like"},
+    {"id":4,"name":"Refined Concrete Chair","type":"follow"}
   ];
 
   constructor( private popoverCtrl: PopoverController) { }
