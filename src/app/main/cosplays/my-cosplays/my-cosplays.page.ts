be59d8cae84367a27845bd26b2ad08e88ddf201a
AppDeveloperVM <vicmr1994@gmail.com>
2020-06-09 19:20:35 +0200

    Update my-cosplays.page.ts

--- src/app/main/cosplays/my-cosplays/my-cosplays.page.ts
+++ src/app/main/cosplays/my-cosplays/my-cosplays.page.ts
@@ -6,6 +6,7 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { PopoverController } from '@ionic/angular';
 import { PopinfoComponent } from 'src/app/components/popinfo/popinfo.component';
 import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-my-cosplays',
@@ -16,7 +17,7 @@ export class MyCosplaysPage implements OnInit {
   loadedCosplays: Cosplay[];
   listedLoadedCosplays: Cosplay[];
   relevantCosplays: Cosplay[];
-  notifications: number;
+  notifications: any[];
 
   constructor(
     private cosplaysService: CosplaysService,
@@ -32,7 +33,7 @@ export class MyCosplaysPage implements OnInit {
 
     this.listedLoadedCosplays = this.loadedCosplays.slice(0);
 
-    this.notifications = 3;
+    this.notifications = [{name: 'John'}, {name: 'John'}, {name: 'John'}];
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
@@ -48,21 +49,25 @@ export class MyCosplaysPage implements OnInit {
     console.log(event.detail);
   }
 
+
   async mostrarPop( event ) {
+    // console.log('notif:' + this.notifications);
+
     const popover = await this.popoverCtrl.create({
       component: PopinfoComponent,
+      componentProps: { notif_count: this.notifications},
       event,
       // mode: 'ios',
       backdropDismiss: true
     });
 
     await popover.present();
-    
-    const { data } = await popover.onWillDismiss(); // onDidDismiss();
 
-    console.log('Padre:' + data);
+    const { data } = await popover.onWillDismiss(); // onDidDismiss();
   }
 
-
+  /* getItems() {
+    return this.http.get<[]>('components/popinfo');
+  }*/
 
 }
