6d097fdef561cdba92079a6b59b4af3de04ad2ba
AppDeveloperVM <vicmr1994@gmail.com>
2020-06-11 20:33:59 +0200

    "edit cosplay" updated
    page - updated
    path - fixed

--- src/app/main/cosplays/my-cosplays/edit-cosplay/edit-cosplay.page.ts
+++ src/app/main/cosplays/my-cosplays/edit-cosplay/edit-cosplay.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CosplaysService } from '../../cosplays.service';
+import { NavController } from '@ionic/angular';
+import { Cosplay } from '../../cosplay.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-cosplay',
@@ -6,10 +11,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-cosplay.page.scss'],
 })
 export class EditCosplayPage implements OnInit {
+  cosplay: Cosplay;
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private cosplayService: CosplaysService,
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('cosplayId')) {
+        this.navCtrl.navigateBack('main/tabs/cosplays/my-cosplays');
+        return;
+      }
+      this.cosplay = this.cosplayService.getCosplay(paramMap.get('cosplayId'));
+      this.form = new FormGroup({
+        characterName: new FormControl(this.cosplay.characterName , {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        }),
+        series: new FormControl(this.cosplay.series, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)]
+        }),
+        description: new FormControl(this.cosplay.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)]
+        })
+      });
+    });
+  }
+
+  onUpdateCosplay() {
+    if (!this.form.valid) {
+      return;
+    }
+    console.log(this.form);
   }
 
 }
