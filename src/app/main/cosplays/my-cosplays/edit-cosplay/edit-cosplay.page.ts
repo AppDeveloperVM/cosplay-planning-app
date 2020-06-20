ddde11ae588e864928f0671b697a17db5ec2436d
AppDeveloperVM <vicmr1994@gmail.com>
2020-06-20 19:04:57 +0200

    Update edit-cosplay.page.ts

--- src/app/main/cosplays/my-cosplays/edit-cosplay/edit-cosplay.page.ts
+++ src/app/main/cosplays/my-cosplays/edit-cosplay/edit-cosplay.page.ts
@@ -13,7 +13,7 @@ import { Subscription } from 'rxjs';
 })
 export class EditCosplayPage implements OnInit, OnDestroy {
   cosplay: Cosplay;
-  cosplaySub: Subscription;
+  private cosplaySub: Subscription;
   form: FormGroup;
 
   constructor(
@@ -30,24 +30,26 @@ export class EditCosplayPage implements OnInit, OnDestroy {
         this.navCtrl.navigateBack('main/tabs/cosplays/my-cosplays');
         return;
       }
-      this.cosplaySub = this.cosplayService.getCosplay(paramMap.get('cosplayId')).subscribe(cosplay => {
+      this.cosplaySub = this.cosplayService
+      .getCosplay(paramMap.get('cosplayId'))
+      .subscribe(cosplay => {
         this.cosplay = cosplay;
+        this.form = new FormGroup({
+          characterName: new FormControl(this.cosplay.characterName , {
+            updateOn: 'blur',
+            validators: [Validators.required]
+          }),
+          series: new FormControl(this.cosplay.series, {
+            updateOn: 'blur',
+            validators: [Validators.required, Validators.maxLength(180)]
+          }),
+          description: new FormControl(this.cosplay.description, {
+            updateOn: 'blur',
+            validators: [Validators.required, Validators.maxLength(180)]
+          })
+        });
       });
 
-      this.form = new FormGroup({
-        characterName: new FormControl(this.cosplay.characterName , {
-          updateOn: 'blur',
-          validators: [Validators.required]
-        }),
-        series: new FormControl(this.cosplay.series, {
-          updateOn: 'blur',
-          validators: [Validators.required, Validators.maxLength(180)]
-        }),
-        description: new FormControl(this.cosplay.description, {
-          updateOn: 'blur',
-          validators: [Validators.required, Validators.maxLength(180)]
-        })
-      });
     });
   }
 
