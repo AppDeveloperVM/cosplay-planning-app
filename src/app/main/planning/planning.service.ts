00067b78bcd4906ab14d5a63a7daeffcf4e5ab42
AppDeveloperV <vicmr1994@gmail.com>
2020-07-23 14:19:58 +0200

    Update planning.service.ts

--- src/app/main/planning/planning.service.ts
+++ src/app/main/planning/planning.service.ts
@@ -2,6 +2,13 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Planning } from './planning.model';
 
+interface PlanningData {
+  title: string;
+  imageUrl: string;
+  places: any;
+  userId: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -23,4 +30,39 @@ export class PlanningService {
     
     return null;
   }
+
+  addPlanning(
+    title: string,
+    imageUrl: string,
+    places: any,
+) {
+    let generatedId: string;
+    const newCosplayGroup = new CosplayGroup(
+        Math.random().toString(),
+        title,
+        series,
+        imageUrl,
+        place,
+        dateFrom,
+        dateTo,
+        this.authService.userId,
+        location
+    );
+    return this.http
+    .post<{title: string}>(
+        'https://cosplay-planning-app.firebaseio.com/cosplay-groups.json',
+        { ...newCosplayGroup, id: null}
+    ).pipe(
+        switchMap(resData => {
+            generatedId = resData.title;
+            return this.cosplaygroups;
+        }),
+        take(1),
+        tap(cosplaygroups => {
+            newCosplayGroup.id = generatedId;
+            this._cosplaygroups.next(cosplaygroups.concat(newCosplayGroup));
+        }));
+
+}
+
 }
