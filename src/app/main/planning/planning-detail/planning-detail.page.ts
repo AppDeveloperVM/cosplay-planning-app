a184f49d8b335084ad028baec3defb61cf6139a5
AppDeveloperVM <vicmr1994@gmail.com>
2020-08-16 22:43:38 +0200

    Updating planning detail page
    basic info
    
    need to change how the location is saved

--- src/app/main/planning/planning-detail/planning-detail.page.ts
+++ src/app/main/planning/planning-detail/planning-detail.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Planning } from '../planning.model';
+import { Subscription } from 'rxjs';
+import { NavController, ModalController, AlertController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PlanningService } from '../planning.service';
+import { Route } from '@angular/compiler/src/core';
+import { MapModalComponent } from 'src/app/shared/map-modal/map-modal.component';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-planning-detail',
@@ -6,10 +14,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planning-detail.page.scss'],
 })
 export class PlanningDetailPage implements OnInit {
+  planning: Planning;
+  planningId: string;
+  private planningSub: Subscription;
+  newPlanning: Planning;
+  isLoading = false;
 
-  constructor() { }
+  constructor(
+    private navCtrl: NavController,
+    private route: ActivatedRoute,
+    private planningService: PlanningService,
+    private modalCtrl: ModalController,
+    private router: Router,
+    private alertCtrl: AlertController
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('planningId')) {
+        this.navCtrl.navigateBack('/main/planning');
+        return;
+      }
+      this.isLoading = true;
+      this.planningId = paramMap.get('planningId');
+      this.planningSub = this.planningService
+      .getPlanning(paramMap.get('planningId'))
+      .subscribe(planning => {
+        this.planning = planning;
+        this.isLoading = false;
+      }, error => {
+        this.alertCtrl
+        .create({
+          header: 'An error ocurred!',
+          message: 'Could not load planning. Try again later.',
+          buttons: [{
+            text: 'Okay',
+            handler: () => {
+              this.router.navigate(['/main/planning']);
+            }
+          }]
+        }).then(alertEl => {
+          alertEl.present();
+        });
+      }
+      );
+
+      console.log('Planning id: ' + this.planning.id);
+    });
   }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) { // if is false
+      return;
+    }
+    const characterName = form.value.character;
+    // this.newCosplay = this.cosplayService.setCosplayRequest();
+
+    console.log(characterName);
+  }
+
+
+
 
 }
