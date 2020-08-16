b3bf85bc4ef27f4fbc069bd748cab877995dafcd
AppDeveloperVM <vicmr1994@gmail.com>
2020-08-16 23:20:53 +0200

    Update planning.model.ts

--- src/app/main/planning/planning.model.ts
+++ src/app/main/planning/planning.model.ts
@@ -1,9 +1,12 @@
+import { PlaceLocation } from '../cosplays/cosplay-groups/location.model';
+
 export class Planning {
   constructor(
     public id: string,
     public title: string,
     public description: string,
     public imageUrl: string,
+    public location: PlaceLocation,
     public places: any[],
     public userId: string
   ) {}
