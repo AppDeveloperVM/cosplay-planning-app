414df5319394bdad2237538fe2e7e316fc0d689e
AppDeveloperVM <vicmr1994@gmail.com>
2020-12-15 19:33:47 +0100

    Profile social data clickable - opens in new tab

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  instagram_url = '';
+  instagram_user = 'SpaceRonin_v';
+  instagram_name = 'SpaceRonin';
+
+  twitter_url = '';
+  twitter_user = 'SpaceRonin_v';
+  twitter_name = 'SpaceRonin';
 
   constructor() { }
 
   ngOnInit() {
+    this.instagram_url = `https://www.instagram.com/${this.instagram_user}/?hl=es`;
+    this.twitter_url = `https://www.twitter.com/${this.twitter_user}/?hl=es`;
   }
 
 }
