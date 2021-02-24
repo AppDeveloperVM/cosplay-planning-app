5d1386fa50d963382d4bbff937fb32b131ac50b2
AppDeveloperV <vicmr1994@gmail.com>
2021-02-24 19:30:52 +0100

    Update my-cosplays.module.ts

--- src/app/main/cosplays/my-cosplays/my-cosplays.module.ts
+++ src/app/main/cosplays/my-cosplays/my-cosplays.module.ts
@@ -7,13 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { MyCosplaysPageRoutingModule } from './my-cosplays-routing.module';
 
 import { MyCosplaysPage } from './my-cosplays.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MyCosplaysPageRoutingModule
+    MyCosplaysPageRoutingModule,
+    RouterModule,
+    SharedModule,
   ],
   declarations: [MyCosplaysPage]
 })
