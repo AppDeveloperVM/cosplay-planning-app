e0e751553992fc527c8345a1a9b560b12305814c
AppDeveloperV <vicmr1994@gmail.com>
2021-02-24 19:30:08 +0100

    updated label component, and includes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { PopoverController } from '@ionic/angular';
+import { PopinfoComponent } from '../popinfo/popinfo.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
+
 export class HeaderComponent implements OnInit {
   @Input() titulo: string;
+  @Input() routeArray: any;
+  notifications: any = [];
+  checked_notif = false;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private popoverCtrl: PopoverController,
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchFileData(); // get notifs from file - this.file_notifications
+  }
+
+  fetchFileData() {
+    fetch('../../assets/data/notifications.json').then(res => res.json())
+      .then(data => {
+        console.log(data.notifications);
+        this.notifications.push(data.notifications);
+
+        //example - this.noticesService.addNotice('SpaceRonin', 'request', 'Cosplay group character request');
+ 
+        });
+  }
+
+  async mostrarPop( event ) {
+    
+    if (true) { // if this.notifications.length > 0
+      console.log('notif:' + this.notifications);
+
+      this.checked_notif = true;
+
+      const popover = await this.popoverCtrl.create({
+        component: PopinfoComponent,
+         componentProps: { notifications: this.notifications},
+        event,
+        // mode: 'ios',
+        backdropDismiss: true
+      });
+      await popover.present();
+  
+      const { data } = await popover.onWillDismiss(); // onDidDismiss();
+    } else {
+      console.log('No notifications - no popup');
+    }
+
+  }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,13 @@ import { MapModalComponent } from './map-modal/map-modal.component';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { ImagePickerComponent } from './pickers/image-picker/image-picker.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [LocationPickerComponent, MapModalComponent, ImagePickerComponent ],
-  imports: [ CommonModule, IonicModule ],
-  exports: [ LocationPickerComponent, MapModalComponent, ImagePickerComponent ],
+  declarations: [LocationPickerComponent, MapModalComponent, ImagePickerComponent,HeaderComponent ],
+  imports: [ CommonModule, IonicModule,RouterModule ],
+  exports: [ LocationPickerComponent, MapModalComponent, ImagePickerComponent,HeaderComponent ],
   entryComponents: [ MapModalComponent]
 })
 
