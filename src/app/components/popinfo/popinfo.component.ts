13adef7d8a12f9faf417554f06b4f985f5917718
AppDeveloperV <vicmr1994@gmail.com>
2021-06-26 17:39:41 +0200

    Implemented reverse ngFor in notifications

--- src/app/components/popinfo/popinfo.component.ts
+++ src/app/components/popinfo/popinfo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 
 @Component({
@@ -6,16 +7,22 @@ import { PopoverController } from '@ionic/angular';
   templateUrl: './popinfo.component.html',
   styleUrls: ['./popinfo.component.scss'],
 })
-export class PopinfoComponent implements OnInit {
+@Pipe({ name: 'reverse' })
+
+export class PopinfoComponent implements OnInit,PipeTransform {
 
   // items = this.notif_count;
 
   @Input() notifications;
 
   constructor( private popoverCtrl: PopoverController) { }
-
+  
   ngOnInit() {
     
+  }
+
+  transform(value) {
+    return value.slice().reverse();
   }
 
 
