f0e58c4fa12c7403015f2c0327e506006c09b88d
AppDeveloperV <vicmr1994@gmail.com>
2021-10-30 17:54:38 +0200

    minor changes

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -12,7 +12,7 @@ import { from, fromEvent, Observable } from 'rxjs';
 export class MapModalComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('map', { static: false }) mapElementRef: ElementRef;
   placesData = [];
-  @Input() center ; // { lat: 39.5695818, lng: 2.6500745 }; // initial route point
+  @Input() center ; // initial route point
   @Input() markers; // = []; // array of markers given
   @Input() selectable; // = true;
   @Input() multiple = false;
