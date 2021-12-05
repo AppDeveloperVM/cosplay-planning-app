00f6f690dd01718019fe01218ed5208ccc557f8d
AppDeveloperVM <vicmr1994@gmail.com>
2021-12-05 12:04:24 +0100

    updated auth service route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { Plugins, Capacitor } from '@capacitor/core';
-import { AuthService } from './auth/auth.service';
+import { AuthService } from './services/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
