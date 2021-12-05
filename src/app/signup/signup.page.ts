61fe33605d7df1a0794648414372e09ee2d8596d
AppDeveloperVM <vicmr1994@gmail.com>
2021-12-05 12:05:48 +0100

    Signup updated

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { Form, NgForm,FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,20 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class SignupPage implements OnInit {
+  ionicForm: FormGroup;
+  isLoading = false;
+  isSubmitted = false;
 
-  name: string = "";
-  email: string = "";
-  password: string = "";
-  confirm_password: string = "";  
+  constructor(public formBuilder: FormBuilder,private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.ionicForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.minLength(2)]],
+      password: ['', [Validators.required, Validators.minLength(2)]],
+      confirm_password: ['', [Validators.required, Validators.minLength(2)]],
+    })
+   }
 
-  ngOnInit() { }
+  onLogin() {
+    this.isLoading = true;
+    this.authService.login();
+    this.loadingCtrl
+    .create({keyboardClose: true, message: 'Logging in..'})
+    .then(loadingEl => {
+      loadingEl.present();
+      setTimeout(() => { 
+        this.isLoading = false;
+        loadingEl.dismiss();
+        this.router.navigateByUrl('/profile');
+      } , 1000);
+     });
+  }
 
-  onSubmit() {
-     alert(
-      this.name + ', ' + this.email + ', ' + this.password + ', ' + this.confirm_password
-     )
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
+
+  onSubmit(form: NgForm) {
+    this.isSubmitted = true;
+    if (!this.ionicForm.valid) {
+      console.log('Please provide all the required values!')
+      return false;
+    } else {
+      console.log(this.ionicForm.value)
+    }
+
   }
 
 }
