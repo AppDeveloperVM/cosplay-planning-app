bea6f4c0359035738ec7b3f3970b735fa8af41af
AppDeveloperVM <vicmr1994@gmail.com>
2021-12-31 17:44:03 +0100

    planning details map modal - changed to leaflet lib - basic

--- src/app/main/planning/planning-detail/planning-detail.page.ts
+++ src/app/main/planning/planning-detail/planning-detail.page.ts
@@ -5,6 +5,9 @@ import { NavController, ModalController, AlertController } from '@ionic/angular'
 import { ActivatedRoute, Router } from '@angular/router';
 import { PlanningService } from '../planning.service';
 import { MapModalComponent } from 'src/app/shared/map-modal/map-modal.component';
+
+import { MapModalLeafletComponent } from 'src/app/shared/map-modal-leaflet/map-modal-leaflet.component';
+
 import { NgForm } from '@angular/forms';
 import { PlaceDataService } from 'src/app/services/place-data.service';
 
@@ -22,92 +25,36 @@ export class PlanningDetailPage implements OnInit, OnDestroy {
   private planningSub: Subscription;
 
   constructor(
+    private modalCtrl: ModalController,
+    private alertCtrl: AlertController,
     private route: ActivatedRoute,
     private router: Router,
     private navCtrl: NavController,
     private planningService: PlanningService,
-    private modalCtrl: ModalController,
-    private alertCtrl: AlertController,
     private placeDataService: PlaceDataService
   ) {
 
    }
 
   ngOnInit() {
-    //this.fetchPlacesData(); //add places from json
-
-    this.route.paramMap.subscribe(paramMap => {
-      if (!paramMap.has('planningId')) {
-        this.navCtrl.navigateBack('/planning');
-        console.log('cant get planningId');
-        return;
-      }
-
-      this.isLoading = true;
-      this.planningId = paramMap.get('planningId');
-      console.log('id:' + paramMap.get('planningId'));
-      console.log(
-        this.planningService.getPlanning(paramMap.get('planningId'))
-      );
-
-      this.planningSub = this.planningService
-      .getPlanning(paramMap.get('planningId'))
-      .subscribe(planning => {
-        this.planning = planning;
-        console.log(this.planning);
-        this.isLoading = false;
-      }, error => {
-        this.alertCtrl
-        .create({
-          header: 'An error ocurred!',
-          message: 'Could not load planning. Try again later.',
-          buttons: [{
-            text: 'Okay',
-            handler: () => {
-              console.log(error);
-              this.router.navigate(['/main/tabs/planning']);
-            }
-          }]
-        }).then(alertEl => {
-          alertEl.present();
-        });
-      }
-      );
-
-    });
+      this.isLoading = false;
   }
 
   ionViewWillEnter() {
     console.log('ionViewWillEnter');
   }
 
-  fetchPlacesData() { // fetch places from json
-    fetch('../../assets/data/places_1.json').then(res => res.json()) // json file depends on planning id
-      .then(data => {
-        this.placesData = data.places;
-        this.placeDataService.setPlaces(this.placesData);
-      });
-  }
-
   onSubmit(form: NgForm) {
-    if (!form.valid) { // if is false
-      return;
-    }
-    const characterName = form.value.character;
-    // this.newCosplay = this.cosplayService.setCosplayRequest();
-
-    console.log(characterName);
   }
 
   onShowFullMap() {
-    this.modalCtrl.create({component: MapModalComponent, componentProps: {
-      center: { lat: this.planning.places['lat'], lng: this.planning.places['lng'] },
-      markers: this.placesData , // array of markers
-      selectable: true,
-      multiple: true,
-      closeButtonText: 'close',
-      title: this.planning.title
-    } }).then(modalEl => {
+    this.modalCtrl.create({component: MapModalLeafletComponent, 
+      componentProps: {
+        markers: this.placesData , // array of markers
+        selectable: true,
+        multiple: true,
+        closeButtonText: 'close',
+      } }).then(modalEl => {
       modalEl.present();
     });
   }
