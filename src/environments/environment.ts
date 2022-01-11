64f6ea0163802d04312dfcb48ff5fa70de2bc671
AppDeveloperVM <vicmr1994@gmail.com>
2022-01-11 10:30:34 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,17 +5,18 @@
 export const environment = {
   production: false,
   googleMapsAPIKey: 'AIzaSyC6A6IeeIRA25u3T3MMI4M84D2bkbMuQXs',
-  firebase: {
-    apiKey: "AIzaSyC6IykPJQ8vddxcsaCVZNqBF_zV9pygqZo",
-    authDomain: "cosplay-planning-app.firebaseapp.com",
-    databaseURL: "https://cosplay-planning-app.firebaseio.com",
-    projectId: "cosplay-planning-app",
-    storageBucket: "cosplay-planning-app.appspot.com",
-    messagingSenderId: "588940181672",
-    appId: "1:588940181672:web:344b35e3ede161f057029d",
-    measurementId: "G-J1SXPNPEZK"
-  }
 };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC6IykPJQ8vddxcsaCVZNqBF_zV9pygqZo",
+  authDomain: "cosplay-planning-app.firebaseapp.com",
+  databaseURL: "https://cosplay-planning-app.firebaseio.com",
+  projectId: "cosplay-planning-app",
+  storageBucket: "cosplay-planning-app.appspot.com",
+  messagingSenderId: "588940181672",
+  appId: "1:588940181672:web:344b35e3ede161f057029d",
+  measurementId: "G-J1SXPNPEZK"
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
