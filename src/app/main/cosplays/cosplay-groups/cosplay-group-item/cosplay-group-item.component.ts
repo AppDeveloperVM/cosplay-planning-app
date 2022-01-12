b2988b05de9df6260899d0e1c7ee1e5698ba6488
AppDeveloperVM <vicmr1994@gmail.com>
2022-01-12 16:17:08 +0100

    update cosgroup item

--- src/app/main/cosplays/cosplay-groups/cosplay-group-item/cosplay-group-item.component.ts
+++ src/app/main/cosplays/cosplay-groups/cosplay-group-item/cosplay-group-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { CosplayGroup } from '../cosplay-group.model';
+import { UploadImageService } from 'src/app/services/upload-img.service';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Component({
   selector: 'app-cosplay-group-item',
@@ -8,13 +10,21 @@ import { CosplayGroup } from '../cosplay-group.model';
 })
 export class CosplayGroupItemComponent implements OnInit, AfterViewInit {
   @Input() cosplaygroup: CosplayGroup;
+  public imgSrc: any;
 
-  constructor() {
+  constructor(
+    private st: AngularFireStorage,
+    private uploadService: UploadImageService) {
   }
 
   ngOnInit() {
     console.log(this.cosplaygroup);
+    let ref = this.st.ref(this.cosplaygroup.imageUrl);
+    this.imgSrc = ref.getDownloadURL();
+    console.log("image: " + this.imgSrc);
   }
+
+  
 
   ngAfterViewInit() {
   }
