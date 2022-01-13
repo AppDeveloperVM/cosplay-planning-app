5c9ea69ecb1571efd3683508c6d8eea82db8c819
AppDeveloperVM <vicmr1994@gmail.com>
2022-01-13 12:29:54 +0100

    cosplayGroupItem updated with navigation data

--- src/app/main/cosplays/cosplay-groups/cosplay-group-item/cosplay-group-item.component.ts
+++ src/app/main/cosplays/cosplay-groups/cosplay-group-item/cosplay-group-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { CosplayGroup } from '../cosplay-group.model';
 import { UploadImageService } from 'src/app/services/upload-img.service';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cosplay-group-item',
@@ -12,21 +13,35 @@ export class CosplayGroupItemComponent implements OnInit, AfterViewInit {
   @Input() cosplaygroup: CosplayGroup;
   public imgSrc: any;
 
+  navigationExtras: NavigationExtras = {
+    state : {
+      cosplaygroup: null
+    }
+  }
+
   constructor(
     private st: AngularFireStorage,
-    private uploadService: UploadImageService) {
+    private uploadService: UploadImageService,
+    private router: Router
+  ) {
+    
   }
 
   ngOnInit() {
     console.log(this.cosplaygroup);
-    let ref = this.st.ref(this.cosplaygroup.imageUrl);
-    this.imgSrc = ref.getDownloadURL();
-    console.log("image: " + this.imgSrc);
   }
 
-  
-
   ngAfterViewInit() {
+  }
+
+  onGoToSee(item: any): void {
+    this.navigationExtras.state.value = item;
+    this.router.navigate(['main/tabs/cosplays/cosplay-groups/cosplay-group-details/'], this.navigationExtras );
+  }
+
+  onGoToEdit(item: any): void {
+    this.navigationExtras.state.value = item;
+    this.router.navigate(['main/tabs/cosplays/cosplay-groups/edit/'], this.navigationExtras );
   }
 
 }
