2ffd9382d3b3be084ef208c199c7547fb7c87228
AppDeveloperVM <vicmr1994@gmail.com>
2022-01-15 19:17:43 +0100

    Update characterMember.model.ts

--- src/app/models/characterMember.model.ts
+++ src/app/models/characterMember.model.ts
@@ -3,7 +3,10 @@ import { User } from "./user.model";
 export class CharacterMember {
     constructor(
         public name: string,
+        public alternativeVersion: boolean,
+        public versionName: string,
         public cosplayerId: string,
         public asistanceConfirmed: boolean,
+        public requestConfirmed: boolean
     ) {}
 }
