60b0baeb4a71ad8cc6c3c8c43015266dd68004f7
AppDeveloperVM <vicmr1994@gmail.com>
2022-01-16 20:36:18 +0100

    Update cosgroup-edit-modal.component.ts

--- src/app/shared/cosgroup-edit-modal/cosgroup-edit-modal.component.ts
+++ src/app/shared/cosgroup-edit-modal/cosgroup-edit-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { NavigationExtras, Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -9,9 +10,22 @@ import { ModalController } from '@ionic/angular';
 })
 export class CosgroupEditModalComponent implements OnInit {
 
+  cosplayGroupMember = null;
+
+  navigationExtras: NavigationExtras = {
+    state : {
+      cosplaygroupmember: null
+    }
+  }
+
   constructor(
-    private modalCtrl: ModalController
-  ) { }
+    private modalCtrl: ModalController,
+    private router: Router
+  ) {
+    const navigation = this.router.getCurrentNavigation();
+    if(navigation?.extras?.state == undefined) { this.router.navigate(['main/tabs/cosplays/cosplay-groups']); }
+    this.cosplayGroupMember = navigation?.extras?.state.value;
+  }
 
   ngOnInit() {}
 
