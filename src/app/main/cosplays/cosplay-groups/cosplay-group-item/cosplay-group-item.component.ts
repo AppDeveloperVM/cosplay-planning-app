85a617ac1258b9394b9d3da5aec3b115b55a789f
AppDeveloperVM <vicmr1994@gmail.com>
2022-01-17 13:19:48 +0100

    updated cosGroupItem - added delete button , changed styles

--- src/app/main/cosplays/cosplay-groups/cosplay-group-item/cosplay-group-item.component.ts
+++ src/app/main/cosplays/cosplay-groups/cosplay-group-item/cosplay-group-item.component.ts
@@ -3,7 +3,8 @@ import { CosplayGroup } from '../cosplay-group.model';
 import { UploadImageService } from 'src/app/services/upload-img.service';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { NavigationExtras, Router } from '@angular/router';
-import { Platform } from '@ionic/angular';
+import { LoadingController, Platform } from '@ionic/angular';
+import { CosplayGroupService } from 'src/app/services/cosplay-group.service';
 
 
 @Component({
@@ -26,7 +27,9 @@ export class CosplayGroupItemComponent implements OnInit, AfterViewInit {
     private st: AngularFireStorage,
     private uploadService: UploadImageService,
     private router: Router,
-    private platform: Platform
+    private platform: Platform,
+    private loadingCtrl: LoadingController,
+    private cosplayGroupService: CosplayGroupService,
   ) {
     
   }
@@ -51,6 +54,28 @@ export class CosplayGroupItemComponent implements OnInit, AfterViewInit {
   onGoToEdit(item: any): void {
     this.navigationExtras.state.value = item;
     this.router.navigate(['main/tabs/cosplays/cosplay-groups/edit/'], this.navigationExtras );
+  }
+
+  async onDeleteCosGroup(cosGroupId: string): Promise<void> {
+
+    await this.loadingCtrl
+    .create({
+      message: 'Deleting Cosplay Group...'
+    })
+    .then(loadingEl => {
+      loadingEl.present();
+        try {
+          this.cosplayGroupService.onDeleteCosGroup(cosGroupId);
+        }catch (err) {
+          console.log(err);
+        }
+
+        setTimeout(() => {
+          loadingEl.dismiss();
+        }, 500);
+
+      //this.router.navigate(['main/tabs/cosplays/cosplay-groups']);
+    });
   }
 
 }
