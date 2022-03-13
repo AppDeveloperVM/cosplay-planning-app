eab3c205c8a7639f311394967f746ef8b2fae89b
VMTechy <vicmr1994@gmail.com>
2022-03-13 23:23:18 +0100

    CosItem updated

--- src/app/main/cosplays/my-cosplays/cosplay-item/cosplay-item.component.ts
+++ src/app/main/cosplays/my-cosplays/cosplay-item/cosplay-item.component.ts
@@ -37,6 +37,9 @@ export class CosplayItemComponent implements OnInit {
     let imageName = this.cosplay.imageUrl;
     this.uploadImgService.getStorageImgUrl(imageName,2).then((val)=>{
       this.imageUrl = val;
+    })
+
+    this.uploadImgService.getStorageImgUrl(imageName,0).then((val)=>{
       this.cosplay.imageUrl = val;
     })
     
