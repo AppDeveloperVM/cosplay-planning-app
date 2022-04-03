361803a4d8e878d89094e38cf1efec991ab021f1
VMTechy <vicmr1994@gmail.com>
2022-04-03 23:29:51 +0200

    Create appSettingsConfig.model.ts

---
+++ src/app/models/appSettingsConfig.model.ts
@@ -0,0 +1,8 @@
+export class appSettingsConfig {
+    constructor(
+        public privateAccount: boolean,
+        public push_notifs: boolean,
+        public theme: string,
+        public darkMode: boolean
+    ) {}
+}
