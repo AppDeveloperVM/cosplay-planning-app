e4481b2296d3599f2cd34718dd1deb9cbdecc9c0
VMTechy <vicmr1994@gmail.com>
2022-05-01 20:44:23 +0200

    update cosTask modal comp

--- src/app/main/cosplays/my-cosplays/cosplay-details/cos-task-modal/cos-task-modal.component.ts
+++ src/app/main/cosplays/my-cosplays/cosplay-details/cos-task-modal/cos-task-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cos-task-modal',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CosTaskModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {}
+
+  onCancel() {
+    this.modalCtrl.dismiss();
+    // cerrar modal
+  }
 
 }
