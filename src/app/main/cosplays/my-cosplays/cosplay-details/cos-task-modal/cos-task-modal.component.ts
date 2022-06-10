c226883eb583be17d4b70e7054d1178d84151f66
AppDeveloperVM <vicmr1994@gmail.com>
2022-06-10 21:52:03 +0200

    update cosTask modal

--- src/app/main/cosplays/my-cosplays/cosplay-details/cos-task-modal/cos-task-modal.component.ts
+++ src/app/main/cosplays/my-cosplays/cosplay-details/cos-task-modal/cos-task-modal.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoadingController, ModalController } from '@ionic/angular';
+import { Cosplay } from 'src/app/models/cosplay.model';
+import { CosTask } from 'src/app/models/cosTask.model';
+import { CosplayDevelopService } from 'src/app/services/cosplay-develop.service';
 
 @Component({
   selector: 'app-cos-task-modal',
@@ -7,10 +12,60 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./cos-task-modal.component.scss'],
 })
 export class CosTaskModalComponent implements OnInit {
+  @Input() selectedCosplay: Cosplay;
 
-  constructor(private modalCtrl: ModalController) { }
+  form: FormGroup;
+  task: CosTask;
 
-  ngOnInit() {}
+  constructor(
+    private modalCtrl: ModalController,
+    private loadingCtrl: LoadingController,
+    private router: Router,
+    private cosDevelopService: CosplayDevelopService
+  ) { }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl('tarea', {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      alarm: new FormControl(false, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      date: new FormControl(new Date() , {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+      done: new FormControl(false, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(180)]
+      }),
+    });
+      
+  }
+
+  onSubmitElement(){
+    if (!this.form.valid) return
+
+    this.loadingCtrl
+    .create({
+      message: 'Creating Task ...'
+    })
+    .then(loadingEl => {
+      loadingEl.present();
+      const task = this.form.value;
+      const cosplayId = this.selectedCosplay?.id || null;
+      this.cosDevelopService.onSaveTask(task, cosplayId);
+      console.log(task);
+      
+      loadingEl.dismiss();
+      this.form.reset();
+
+      this.modalCtrl.dismiss();
+    });
+  }
 
   onCancel() {
     this.modalCtrl.dismiss();
