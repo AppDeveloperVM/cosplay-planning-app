de42631e1090687d606b8aa130bc021d5b91483e
VMTechy <vicmr1994@gmail.com>
2022-10-11 15:48:49 +0200

    Update notices.service.ts

--- src/app/services/notices.service.ts
+++ src/app/services/notices.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { Notice } from '../models/notice.model';
+import { notifData } from '../models/notifData.model';
+import { DataService } from './data.service';
 
 interface NoticeData {
   userFrom: string;
@@ -14,14 +16,13 @@ interface NoticeData {
 export class NoticesService {
   _notices$ = new Subject<Notice[]>();
   noticeList = [];
+  public notifObj : notifData;
   
   get notices$(): Observable<Notice[]> {
     return this._notices$.asObservable();
   }
 
-  constructor() {
-    
-    
+  constructor(private dataService: DataService) { 
   }
 
   addNotice(
@@ -72,16 +73,41 @@ export class NoticesService {
     return false;
   }
 
-  fetchFileData() {
+ /*  fetchFileData() {
 
     fetch('../../assets/data/notifications.json')
     .then(res => res.json())
     .then(data => {
       //console.log("Fetching Notifications from json..");
       this.setNotices(data.notifications);
+      console.log(data.notifications);
     });
 
+  }  */
+
+  async loadLocalData(key){
+    console.log('->Load local data');
+    
+    //await this.dataService.addData('user',`Vic ${Math.floor(Math.random() * 100)}`);
+    this.dataService.getData(key).subscribe(async notifications => {
+      if(notifications != null){
+        console.log(notifications);
+        //this.setNotices(notifications);
+      } else {
+        await this.addNotificationTest();
+      }
+      this.setNotices(notifications);
+    });
   }
-   
+
+  async addNotificationTest() {
+    console.log('-> Triggered update');
+    this.notifObj = new notifData('user', 'cosplay', 'request', 'notificacion de prueba');
+    //await this.settings._settings$.next(this.settingsObj);
+
+    this.dataService.addData('notifs', this.notifObj, true);
+    console.log(this.notifObj);
+    //await this.dataService.addData(LOCALDATAKEY, this.settingsObj, true);
+  }
 
 }
