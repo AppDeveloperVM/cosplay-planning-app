a76e885bf2fbaaad691f77f94219005f8170fd8d
VMTechy <vicmr1994@gmail.com>
2022-10-13 11:48:48 +0200

    form cosToMake - added validations and error msg

--- src/app/main/cosplays/my-cosplays/cosplay-details/cos-element-tomake-modal/cos-element-tomake-modal.component.ts
+++ src/app/main/cosplays/my-cosplays/cosplay-details/cos-element-tomake-modal/cos-element-tomake-modal.component.ts
@@ -18,8 +18,9 @@ export class CosElementTomakeModalComponent implements OnInit {
   @Input() item;
   @Input() itemID;
 
-  form: FormGroup;
+  element: FormGroup;
   cosElementToMake: CosElementToDo;
+  validations = null;
 
   constructor(
     private modalCtrl: ModalController,
@@ -28,33 +29,52 @@ export class CosElementTomakeModalComponent implements OnInit {
     private cosDevelopService: CosplayDevelopService,
     public fb: FormBuilder
   ) {
+
+    this.validations = {
+      'name': [
+        { type: 'required', message: 'Name is required.' },
+        { type: 'maxlength', message: 'Name cannot be more than 25 characters long.' },
+      ],
+      'hours': [
+        { type: 'maxlength', message: 'Cost cannot be more than 2 digits long.' },
+        { type: 'pattern', message: 'The cost must contain only numbers .' },
+      ],
+      'minutes': [
+        { type: 'maxlength', message: 'Cost cannot be more than 2 digits long.' },
+        { type: 'pattern', message: 'The cost must contain only numbers .' },
+      ],
+      'notes': [
+        { type: 'maxlength', message: 'Cost cannot be more than 2 digits long.' },
+      ],
+      // other validations
+    };
     
   }
 
   ngOnInit() {
-    this.form =  this.fb.group({
+    this.element =  this.fb.group({
       name: new FormControl( this.item?.name ? this.item?.name : null, {
         updateOn: 'blur',
         validators: [Validators.required, Validators.maxLength(180)]
       }),
       hours: new FormControl( this.item?.hours ? this.item?.hours : '00' , {
         updateOn: 'blur',
-        validators: [Validators.required, Validators.maxLength(180)]
+        validators: [Validators.maxLength(2)]
       }),
       minutes: new FormControl( this.item?.minutes ? this.item?.minutes : '01' , {
         updateOn: 'blur',
-        validators: [Validators.required, Validators.maxLength(180)]
+        validators: [Validators.maxLength(2)]
       }),
       notes: new FormControl(this.item?.notes ? this.item?.notes : 'notes', {
         updateOn: 'blur',
-        validators: [Validators.required, Validators.maxLength(180)]
+        validators: [Validators.maxLength(180)]
       }),
       elementID: new FormControl( this.itemID )
     });
   }
 
   onSubmitElement(){
-    if (!this.form.valid) return
+    if (!this.element.valid) return
 
     this.loadingCtrl
     .create({
@@ -62,13 +82,13 @@ export class CosElementTomakeModalComponent implements OnInit {
     })
     .then(loadingEl => {
       loadingEl.present();
-      const elementToMake = this.form.value;
+      const elementToMake = this.element.value;
       const cosplayId = this.selectedCosplay?.id || null;
       this.cosDevelopService.onSaveElToMake(elementToMake, cosplayId);
       console.log(elementToMake);
       
       loadingEl.dismiss();
-      this.form.reset();
+      this.element.reset();
 
       this.modalCtrl.dismiss();
     });
