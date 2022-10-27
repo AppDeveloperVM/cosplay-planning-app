c4de3b6cdc2992f61a283d9df8c1d22cdad30b98
AppDeveloperVM <vicmr1994@gmail.com>
2022-10-27 17:19:12 +0200

    Update authenticationService.ts
    updated SendVerifyLink function, added completeAccesWithEmailLink

--- src/app/services/authenticationService.ts
+++ src/app/services/authenticationService.ts
@@ -10,7 +10,7 @@ import {
 } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { getAuth } from 'firebase/auth';
+import { getAuth, isSignInWithEmailLink, sendSignInLinkToEmail, signInWithEmailLink } from 'firebase/auth';
 import { resolve } from 'path';
 import { rejects } from 'assert';
 import { AlertController } from '@ionic/angular';
@@ -206,8 +206,34 @@ export class AuthenticationService {
   }
   // Email verification when new user register
   SendVerificationMail() {
+
+    const actionCodeSettings = {
+      // URL you want to redirect back to. The domain (www.example.com) for
+      // this URL must be whitelisted in the Firebase Console.
+      url: 'https://cosplay-planning-app.vercel.app/verified-email',
+      // This must be true for email link sign-in.
+      handleCodeInApp: true,
+    };
+
     return this.ngFireAuth.currentUser.then((user) => {
-      return user.sendEmailVerification({
+
+      const email = user.email;
+      const auth = getAuth();
+
+      sendSignInLinkToEmail(auth, email, actionCodeSettings)
+        .then(() => {
+          // The link was successfully sent. Inform the user.
+          // Save the email locally so you don't need to ask the user for it again
+          // if they open the link on the same device.
+          window.localStorage.setItem('emailForSignIn', email);
+          // ...
+        }).catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ...
+        });
+      
+      /* return user.sendEmailVerification({
         url : "https://cosplay-planning-app.vercel.app/verified-email",
         handleCodeInApp: true,
       }).then(() => {
@@ -215,8 +241,44 @@ export class AuthenticationService {
       }).catch((error) => {
         console.log('error catched 3');
         window.alert(error.message)
-      })
+      }) */
+
     });
+  }
+
+  CompleteAccessWithEmailLink(){
+    // Confirm the link is a sign-in with email link.
+    const auth = getAuth();
+    if (isSignInWithEmailLink(auth, window.location.href)) {
+      // Additional state parameters can also be passed via URL.
+      // This can be used to continue the user's intended action before triggering
+      // the sign-in operation.
+      // Get the email if available. This should be available if the user completes
+      // the flow on the same device where they started it.
+      let email = window.localStorage.getItem('emailForSignIn');
+      if (!email) {
+        // User opened the link on a different device. To prevent session fixation
+        // attacks, ask the user to provide the associated email again. For example:
+        email = window.prompt('Please provide your email for confirmation');
+      }
+
+      // The client SDK will parse the code from the link for you.
+      signInWithEmailLink(auth, email, window.location.href)
+        .then((result) => {
+          // Clear email from storage.
+          window.localStorage.removeItem('emailForSignIn');
+          // You can access the new user via result.user
+          // Additional user info profile not available via:
+          // result.additionalUserInfo.profile == null
+          // You can check if the user is new or existing:
+          // result.additionalUserInfo.isNewUser
+        })
+        .catch((error) => {
+          // Some error occurred, you can inspect the code: error.code
+          // Common errors could be invalid email and invalid or expired OTPs.
+        });
+
+      }
   }
 
   ChangeToVerifiedAccount() : Promise<any>{
