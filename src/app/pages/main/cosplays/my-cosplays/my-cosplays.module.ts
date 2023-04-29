667637f40ec6f22ce06c93b0f391796839fc9b1b
VMTechy <vicmr1994@gmail.com>
2023-04-29 12:11:02 +0200

    Update my-cosplays.module.ts

--- src/app/pages/main/cosplays/my-cosplays/my-cosplays.module.ts
+++ src/app/pages/main/cosplays/my-cosplays/my-cosplays.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ToastController } from '@ionic/angular';
 
 import { MyCosplaysPageRoutingModule } from './my-cosplays-routing.module';
 
@@ -10,7 +10,44 @@ import { MyCosplaysPage } from './my-cosplays.page';
 import { SharedModule } from '../../../../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { CosplayItemComponent } from './cosplay-item/cosplay-item.component';
-import { LazyLoadImageModule } from 'ng-lazyload-image';
+import { LazyLoadImageModule, LAZYLOAD_IMAGE_HOOKS, IntersectionObserverHooks, Attributes } from 'ng-lazyload-image';
+import { from, ObservableInput } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+
+@Injectable()
+export class LazyLoadImageHooks extends IntersectionObserverHooks {
+  
+  constructor(private toastCtrl : ToastController) {
+    super();
+  }
+
+  setup(attributes : Attributes){
+    attributes.offset = 10;
+    attributes.defaultImagePath = './../../assets/avatar.png';
+    attributes.errorImagePath = './../../assets/avatar.png';
+    return super.setup(attributes);
+  }
+
+  /* loadImage({ imagePath }: Attributes): Promise<string> {
+    return fetch(imagePath, {
+      headers: {
+        Authorization: 'Bearer ...',
+        'Access-Control-Allow-Headers': '*'
+      },
+    })
+      .then((res) => res.blob())
+      .then((blob) => URL.createObjectURL(blob));
+  } */
+
+  
+
+  /* async loadImage({ imagePath }: Attributes): Promise<string> {
+    return await fetch(imagePath)
+      .then((res) => res.blob())
+      .then((blob) => URL.createObjectURL(blob));
+  } */
+}
 
 @NgModule({
   imports: [
@@ -23,6 +60,8 @@ import { LazyLoadImageModule } from 'ng-lazyload-image';
     SharedModule,
     LazyLoadImageModule
   ],
-  declarations: [MyCosplaysPage, CosplayItemComponent]
+  declarations: [MyCosplaysPage, CosplayItemComponent],
+  providers: [{ provide: LAZYLOAD_IMAGE_HOOKS, useClass: LazyLoadImageHooks }],
+
 })
 export class MyCosplaysPageModule {}
